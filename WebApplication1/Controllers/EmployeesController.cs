96e5f1910842e42d28ba24a61f949e311fc2a632
AsadMughalAM <am127955@gmail.com>
2024-08-07 23:01:58 +0500

    Add project files.

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var EmployeeList=new List<Employee> {  
+                new Employee{Id=1,Name="Asad",Address="LHR"},            
+                new Employee{Id=2,Name="Huzaifa",Address="ISD"},            
+                new Employee{Id=3,Name="Abdullah",Address="KPK"},            
+            };
+            return Ok(EmployeeList);
+        }
+    }
+}
